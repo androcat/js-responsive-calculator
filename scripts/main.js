abd97526d44c2488c9d18f0944ca88706580040d
Jay Medina <jay.medina.celis@gmail.com>
2023-05-05 12:33:56 -0400

    Adds alert for clear button

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,20 @@
 (function () {
   const numberButtonsAll = document.querySelectorAll(".number");
   const operatorButtonsAll = document.querySelectorAll(".operator");
+  const clearButton = document.querySelector(".clear");
   console.log(operatorButtonsAll);
 
   function pushNumber() {
     numberButtonsAll.forEach((button) =>
-      button.addEventListener("click", function () {
-        alert(button.value);
-      })
+      button.addEventListener("click", () => alert(button.value))
     );
   }
 
   function pushOperator() {
     operatorButtonsAll.forEach((button) =>
-      button.addEventListener("click", function () {
-        alert(button.value);
-      })
+      button.addEventListener("click", () => alert(button.value))
     );
+    clearButton.addEventListener("click", () => alert(clearButton.value));
   }
 
   pushNumber();
