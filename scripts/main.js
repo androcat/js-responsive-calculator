b95150620601f813c870ad42bc8e7f9fed70a929
Jay Medina <jay.medina.celis@gmail.com>
2023-05-05 12:13:31 -0400

    Alerts when a number is button is pushed

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
+(function () {
+  const numberButtonsAll = document.querySelectorAll(".number");
+  console.log(numberButtonsAll);
 
+  function pushNumber() {
+    numberButtonsAll.forEach((numButton) =>
+      numButton.addEventListener("click", function () {
+        alert("You pushed a number!");
+      })
+    );
+  }
+
+  pushNumber();
+})();
