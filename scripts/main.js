23c0aeff0a0cde13ee4681b4c6f0551c7bb287a4
Jay Medina <jay.medina.celis@gmail.com>
2023-05-05 13:35:42 -0400

    Adds pressed numbers and operators into an array

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,24 @@
   const clearButton = document.querySelector(".clear");
   const equalButton = document.querySelector(".equal-sign");
 
+  let calculationArr = [];
+
   function pushNumber() {
     numberButtonsAll.forEach((button) =>
-      button.addEventListener("click", () => alert(button.value))
+      button.addEventListener("click", () => {
+        alert(button.value);
+        calculationArr.push(button.value);
+      })
     );
+    console.log(calculationArr);
   }
 
   function pushOperator() {
     operatorButtonsAll.forEach((button) =>
-      button.addEventListener("click", () => alert(button.value))
+      button.addEventListener("click", () => {
+        alert(button.value);
+        calculationArr.push(button.value);
+      })
     );
     clearButton.addEventListener("click", () => alert(clearButton.value));
   }
@@ -24,4 +33,5 @@
   pushNumber();
   pushOperator();
   calculate();
+  setInterval(console.log(calculationArr), 1000);
 })();
