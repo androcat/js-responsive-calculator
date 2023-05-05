523ffe8ff0b706da9b9d887eba681dee07ec87e8
Jay Medina <jay.medina.celis@gmail.com>
2023-05-05 14:35:50 -0400

    Finds numbers input into calculator and the operator given

--- scripts/main.js
+++ scripts/main.js
@@ -6,32 +6,44 @@
 
   let calculationArr = [];
 
-  function pushNumber() {
-    numberButtonsAll.forEach((button) =>
-      button.addEventListener("click", () => {
-        alert(button.value);
-        calculationArr.push(button.value);
-      })
-    );
+  function pushNumber(button) {
+    alert(button.value);
+    calculationArr.push(button.value);
+    // delete later
     console.log(calculationArr);
   }
 
-  function pushOperator() {
-    operatorButtonsAll.forEach((button) =>
-      button.addEventListener("click", () => {
-        alert(button.value);
-        calculationArr.push(button.value);
-      })
-    );
-    clearButton.addEventListener("click", () => alert(clearButton.value));
+  function pushOperator(button) {
+    alert(button.value);
+    calculationArr.push(button.value);
+    // delete later
+    console.log(calculationArr);
   }
 
   function calculate() {
-    equalButton.addEventListener("click", () => alert(equalButton.value));
-  }
+    let num1, num2, opIndex, op;
+    for (let i = 0; i < calculationArr.length; i++) {
+      if ("*/+_".includes(calculationArr[i])) {
+        opIndex = i;
+        op = calculationArr[i];
+        break;
+      }
+    }
+    console.log({ calculationArr });
+    console.log({ opIndex });
+    num1 = calculationArr.slice(0, opIndex).join("");
+    num2 = calculationArr.slice(opIndex + 1).join("");
 
-  pushNumber();
-  pushOperator();
-  calculate();
-  setInterval(console.log(calculationArr), 1000);
+    console.log("num1", num1);
+    console.log("num2", num2);
+    console.log("op", op);
+  }
+  numberButtonsAll.forEach((button) =>
+    button.addEventListener("click", () => pushNumber(button))
+  );
+  operatorButtonsAll.forEach((button) =>
+    button.addEventListener("click", () => pushOperator(button))
+  );
+  clearButton.addEventListener("click", () => alert(clearButton.value));
+  equalButton.addEventListener("click", () => calculate());
 })();
