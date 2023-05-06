af86f2bdedcf3fcbd3cbd08f8bf4ca73e67a0a84
Jay Medina <jay.medina.celis@gmail.com>
2023-05-06 14:52:07 -0400

    Alerts inputed simple calculation and alerts it as well as the total

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,15 @@
   const equalButton = document.querySelector(".equal-sign");
 
   let calculationArr = [];
+  let total = 0;
 
   function pushNumber(button) {
-    alert(button.value);
     calculationArr.push(button.value);
     // delete later
     console.log(calculationArr);
   }
 
   function pushOperator(button) {
-    alert(button.value);
     calculationArr.push(button.value);
     // delete later
     console.log(calculationArr);
@@ -29,14 +28,29 @@
         break;
       }
     }
-    console.log({ calculationArr });
-    console.log({ opIndex });
-    num1 = calculationArr.slice(0, opIndex).join("");
-    num2 = calculationArr.slice(opIndex + 1).join("");
+    // console.log({ calculationArr });
+    // console.log({ opIndex });
+    num1 = parseInt(calculationArr.slice(0, opIndex).join(""));
+    num2 = parseInt(calculationArr.slice(opIndex + 1).join(""));
 
     console.log("num1", num1);
     console.log("num2", num2);
     console.log("op", op);
+
+    alert(num1 + op + num2);
+
+    if (op === "*") {
+      total = num1 * num2;
+    } else if (op === "/") {
+      //  look up if just integer division
+      total = num1 / num2;
+    } else if (op === "+") {
+      total = num1 + num2;
+    } else if (op === "-") {
+      total = num1 - num2;
+    }
+
+    alert(total);
   }
   numberButtonsAll.forEach((button) =>
     button.addEventListener("click", () => pushNumber(button))
@@ -44,6 +58,6 @@
   operatorButtonsAll.forEach((button) =>
     button.addEventListener("click", () => pushOperator(button))
   );
-  clearButton.addEventListener("click", () => alert(clearButton.value));
+  clearButton.addEventListener("click", () => (calculationArr = []));
   equalButton.addEventListener("click", () => calculate());
 })();
